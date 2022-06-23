940e8c5a8029947c60bbb955573b6612074f1a41
MahdiMF <mmf1373py@gmail.com>
2022-06-23 19:44:23 +0430

    add login and signup with tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
+
+
+class SignUpFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(username='user1')
+
+    def test_signup_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
